4852ffd9e9346eaff6baf259bf340e2899164364
Vladislav <rommel66699@gmail.com>
2024-07-19 13:05:11 +0900

    microservice init

--- src/lib/components/Headers.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    export let title = "header";
-</script>
-
-
-<div class="flex flex-col bg-cinema-secondary w-full items-center">
-    <h2 class="text-cinema-text font-bold text-xl p-4">{title}</h2>
-</div>
