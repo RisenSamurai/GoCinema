816622b0682e076a4d1790c6795778a378acd02f
Vladislav <rommel66699@gmail.com>
2024-07-25 16:08:11 +0900

    Redis: fetch_tmdb.go query caching

--- rating-service/queries/fetch_tmdb.go
+++ rating-service/queries/fetch_tmdb.go
@@ -1,14 +1,25 @@
 package queries
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"github.com/joho/godotenv"
 	"io"
 	"log"
 	"net/http"
 	"os"
 )
+
+var redisClient *redis.Client
+
+func init() {
+	// Initialize Redis client
+	redisClient = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379", // Redis server address
+	})
+}
 
 type TMDBMovie struct {
 	ID     string  `json:"id"`
@@ -20,11 +31,20 @@ func FetchRating(c *gin.Context) {
 	log.Println("Inside FetchRating")
 	id := c.Param("id")
 
+	ctx := c.Request.Context()
+	cachedData, err := redisClient.Get(ctx, "movie:"+id).Result()
+	if err == nil {
+		c.Data(200, "application/json; charset=utf-8", []byte(cachedData))
+		return
+	} else if !errors.Is(err, redis.Nil) {
+		log.Printf("Redis error: %v", err)
+	}
+
 	url := "https://api.themoviedb.org/3/movie/" + id + "?language=en-US"
 
 	req, _ := http.NewRequest("GET", url, nil)
 
-	err := godotenv.Load("config/.env")
+	err = godotenv.Load("config/.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
