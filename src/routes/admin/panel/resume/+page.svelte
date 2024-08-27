2c071b929d8a8f8d11644d568a76ac18b6b2c5d9
Vladislav <rommel66699@gmail.com>
2024-08-27 19:25:14 +0900

    Fixed Gateway

--- src/routes/admin/panel/resume/+page.svelte
+++ src/routes/admin/panel/resume/+page.svelte
@@ -1,45 +1,156 @@
-<section class="flex bg-white w-full h-full ">
-
-
-    <div class="flex flex-col p-2 m-2 h-full w-2/3 rounded-lg">
+<section class="flex bg-white w-full">
+    <div class="flex flex-col p-2 m-2 w-2/3 rounded-lg">
         <div class="flex p-2 mb-2 border-b-2 border-sky-300 justify-between">
             <div class="flex flex-col">
-                <h1 class="font-bold text-3xl text-sky-400">Vladislav Boichuk</h1>
-                <h2 class="font-semibold text-xl text-slate-400">Full Stack Web Developer</h2>
-                <h2 class="font-bold text-xl text-sky-400 mt-6">Work experience</h2>
+                <h1 class="font-bold text-3xl text-sky-400 leading-6">Vladislav Boichuk</h1>
+                <h2 class="font-semibold text-xl text-slate-600 leading-6">Full Stack Web Developer</h2>
+                <h2 class="font-bold text-xl text-sky-400 leading-6 mt-6">Work experience</h2>
             </div>
-
             <div class="flex w-12 h-14">
                 <img class="object-contain" src="/static/go.png" alt="">
             </div>
         </div>
-
-        <div class="flex flex-col h-full rounded-lg p-2">
-
+        <div class="flex flex-col rounded-lg p-2">
             <div class="flex flex-col">
                 <div class="flex flex-col">
-                    <span class="font-bold text-slate-400">NeoSoft(ネオソフト), Nagoya, Japan</span>
-                    <p class="text-slate-500">IT outsourcing company working for Mitsubishi (insurance) with 10+ employees</p>
+                    <span class="font-bold text-slate-600 leading-6">NeoSoft(ネオソフト), Nagoya, Japan (On site)</span>
+                    <p class="text-slate-700 leading-6">IT outsourcing company working for Mitsubishi(insurance) with 10+ employees</p>
                 </div>
                 <div class="flex justify-between mt-2">
-                    <span class="text-slate-500">Full-stuck web developer</span>
-                    <span class="italic text-slate-400 font-bold">10/4/2023 - 3/14/2024</span>
+                    <span class="text-slate-700 leading-6">Full stack web developer(Go, Svelte, MongoDB, TailwindCSS)</span>
+                    <span class="italic text-slate-600 font-bold leading-6">10/4/2023 - 3/14/2024</span>
                 </div>
+                <ul class="list-disc list-inside mt-2 text-slate-600 leading-6 ml-6">
+                    <li class="mb-2">Streamlined user onboarding by developing an automated QR code generation and distribution system, reducing manual data entry by 90% and saving end-users an average of 5 minutes per registration.</li>
+                    <li class="mb-2">Designed and implemented a scalable database schema using MongoDB, storing data for 3 million users with varying permissions and account types, resulting in a 30% reduction in server query time and a 25% cost savings on server resources.</li>
+                    <li class="mb-2">Developed an AI-powered document reading system using TensorFlow, automating user registration and verification, and reducing manual verification time by 80%.</li>
+                    <li class="mb-2">Implemented a secure RestAPI and API Gateway with JWT tokenization, ensuring a 99.99% uptime and protecting user data with enterprise-grade security.</li>
+                    <li class="mb-2">Automated database filling with user ID numbers and daily backups using Go concurrency, saving an estimated 2 hours of manual labor per day for other professionals and reducing the risk of data loss by 95%.</li>
+                </ul>
             </div>
+        </div>
+        <div class="flex flex-col rounded-lg p-2">
+            <div class="flex flex-col">
+                <div class="flex flex-col">
+                    <span class="font-bold text-slate-600 leading-6">Supplymer, Mariupol, Ukraine (Remote)</span>
+                    <p class="text-slate-700 leading-6">Online store of supplements</p>
+                </div>
+                <div class="flex justify-between mt-2">
+                    <span class="text-slate-700 leading-6">Full stack(PHP7, JavaScript, MySQL)</span>
+                    <span class="italic text-slate-600 font-bold leading-6">10/14/2021 - 2/6/2022</span>
+                </div>
+                <ul class="list-disc list-inside mt-2 text-slate-600 leading-6 ml-6">
+                    <li class="mb-2">Designed and implemented a user-friendly interface layout, incorporating interactive elements that improved user engagement by 30% and simplified navigation.</li>
+                    <li class="mb-2">Developed a comprehensive database schema, centralizing goods, orders, and user data, and enabling efficient data management and analysis. </li>
+                    <li class="mb-2">Integrated APIs of leading payment systems, expanding payment options and increasing transaction success rates by 20%.</li>
+                    <li class="mb-2">Built a custom backend from scratch using MVC technology, ensuring a scalable and maintainable architecture, and reducing development time for future enhancements by 40%.</li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex flex-col rounded-lg p-2 bg-white">
+            <div class="flex flex-col">
+                <div class="flex flex-col">
+                    <span class="font-bold text-slate-600 leading-6">MVExchange, Tel-Aviv, Israel (Remote)</span>
+                    <p class="text-slate-700 leading-6">Crypto exchange company</p>
+                </div>
+                <div class="flex justify-between mt-2">
+                    <span class="text-slate-700 leading-6">Back end developer(PHP7, JavaScript, FetchJS)</span>
+                    <span class="italic text-slate-600 font-bold leading-6">9/1/2021 - 10/5/2021</span>
+                </div>
+                <ul class="list-disc list-inside mt-2 text-slate-600 leading-6 ml-6">
+                    <li class="mb-2">Optimized the base API by migrating to RestAPI, achieving a 40% increase in server response speed and enabling faster data exchange. </li>
+                    <li class="mb-2">Integrated Binance crypto exchange API, providing real-time currency rate updates and enhancing the user experience with accurate and up-to-date market data. </li>
+                    <li class="mb-2">Developed a customizable notification system, automating email notifications and improving user engagement by 25% through timely and relevant updates.</li>
+                    <li class="mb-2">Refactored the service architecture to MVC format, improving code maintainability by 50% and enabling faster development of new features. </li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex border-b border-sky-400 pb-2">
+            <h2 class="font-bold text-xl text-sky-400 leading-6 mt-6">Previous experience</h2>
+        </div>
+        <div class="flex flex-col">
+            <div class="flex justify-between">
+                <div class="flex">
+                    <span class="font-bold text-slate-700 leading-6">Junior Back End Developer,</span>
+                    <span class="text-slate-600">Internet Provider "Format Mariupol"</span>
+                </div>
 
+                <span class="italic text-slate-700 font-bold leading-6">12/2018 - 11/2020</span>
+            </div>
         </div>
     </div>
+    <div class="flex flex-col bg-slate-100 p-2 rounded-lg m-2 w-64">
+        <div class="flex border-b border-sky-400 pb-2">
+            <h2 class="font-bold text-l leading-6 uppercase text-sky-400">Contact</h2>
+        </div>
+        <div class="flex">
+            <ul class="list-disc list-inside mt-2 text-slate-600 leading-6 text-sm">
+                <li>Littleton, Colorado, United States</li>
+                <li>+18-000-00-00</li>
+                <li><a href="mailto:boichukworkua@gmail.com">boichukworkua@gmail.com</a></li>
+                <li><a href="www.linkedin.com/in/vladislav-boichuk-5b0b33206">www.linkedin.com/in/vladislav-boichuk-5b0b33206</a></li>
+                <li><a href="https://github.com/RisenSamurai">github.com/RisenSamurai</a></li>
 
-    <div class="flex flex-col bg-slate-200 p-2 rounded-lg m-2 w-56">
-        <h2 class="font-bold text-xl">Contact</h2>
-        <div class="flex border-t border-gray-200">
-            <ul class="flex flex-col list-disc p-4">
-                <li class="text-sm">Phone</li>
-                <li class="text-sm"><a href="mailto:boichukworkua@gmail.com">boichukworkua@gmail.com</a></li>
-                <li class="text-sm">Littleton, Colorado, United States</li>
             </ul>
         </div>
+
+        <div class="flex border-b border-sky-400 pb-2 mt-4">
+            <h2 class="font-bold text-l leading-6 uppercase text-sky-400">Skills</h2>
+        </div>
+        <div class="flex flex-col">
+            <span class="mt-2 text-l text-slate-400">Hard Skills</span>
+            <ul class="list-disc list-inside mt-2 text-slate-600 leading-6 text-sm ml-4">
+                <li>Data design</li>
+                <li>Work with different types of APIs</li>
+                <li>Server side design</li>
+                <li>Site layout</li>
+                <li>Data security</li>
+            </ul>
+        </div>
+        <div class="flex flex-col">
+            <span class="mt-2 text-l text-slate-400">Techniques</span>
+            <ul class="list-disc list-inside mt-2 text-slate-600 leading-6 text-sm ml-4">
+                <li>API Integration</li>
+                <li>API Gateway</li>
+                <li>REST API</li>
+                <li>JWT</li>
+            </ul>
+        </div>
+        <div class="flex flex-col">
+            <span class="mt-2 text-l text-slate-400">Tools and Software</span>
+            <ul class="list-disc list-inside mt-2 text-slate-600 leading-6 text-sm ml-4">
+                <li>Golang</li>
+                <li>MongoDB</li>
+                <li>NoSQL</li>
+                <li>Svelte/SvelteKit</li>
+                <li>TailWindCSS</li>
+            </ul>
+        </div>
+        <div class="flex flex-col">
+            <span class="mt-2 text-l text-slate-400">Languages</span>
+            <ul class="list-disc list-inside mt-2 text-slate-600 leading-6 text-sm ml-4">
+                <li>Ukrainian(Native)</li>
+                <li>Russian(Native)</li>
+                <li>English(Professional)</li>
+                <li>Japanese(Conversational)</li>
+            </ul>
+        </div>
+
+        <div class="flex border-b border-sky-400 pb-2 mt-4">
+            <h2 class="font-bold text-l leading-6 uppercase text-sky-400">Education</h2>
+        </div>
+        <div class="flex flex-col">
+            <span class="font-bold text-slate-600 leading-6 mt-2">IT Step Computer Academy</span>
+            <span class="text-sm text-slate-600">Computer Science</span>
+            <span class="text-sm text-slate-600">Web Technologies</span>
+            <span class="text-sm text-slate-600">Mariupol, Ukraine</span>
+            <span class="text-sm text-slate-600 italic">2015-2018</span>
+        </div>
+        <div class="flex border-b border-sky-400 pb-2 mt-4">
+            <h2 class="font-bold text-l leading-6 uppercase text-sky-400">Other</h2>
+        </div>
+        <div class="flex flex-col">
+            <span class="text-slate-600 leading-6 mt-2">CISCO A+ (2016)</span>
+        </div>
     </div>
-
-
 </section>
