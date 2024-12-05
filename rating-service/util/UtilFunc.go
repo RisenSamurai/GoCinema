607eb00c6be78687ca34d0ebedb5bbc3ab3cfa3d
Wodan <rommel66699@gmail.com>
2024-12-05 12:20:42 -0700

    Added new controller for tv shows

--- rating-service/util/UtilFunc.go
+++ rating-service/util/UtilFunc.go
@@ -1,29 +1,48 @@
 package util
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
-type Genre struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+func FilterData[T any](data interface{}, fields []string) (map[string]interface{}, error) {
+	// Ensure input is a map[string]interface{}
+	rawMap, ok := data.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("input data is not a map")
+	}
+
+	// Filter the map to include only specified fields
+	filteredItem := make(map[string]interface{})
+	for _, field := range fields {
+		if value, exists := rawMap[field]; exists {
+			filteredItem[field] = value
+		}
+	}
+
+	return filteredItem, nil
 }
 
-func ParseGenres[T any](data interface{}) ([]T, error) {
-	// Assert that data is a slice of interfaces
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal input data: %v", err)
+func FilterListData[T any](data interface{}, fields []string) ([]map[string]interface{}, error) {
+	// Ensure input is a map[string]interface{}
+	rawMap, ok := data.([]map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("input data is not a map")
 	}
 
-	// Unmarshal the JSON data into a slice of the target struct type
-	var result []T
-	err = json.Unmarshal(jsonData, &result)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal into target struct slice: %v", err)
+	// Filter the map to include only specified fields
+	var filteredItems []map[string]interface{}
+
+	for _, item := range rawMap {
+		filteredItem := make(map[string]interface{})
+
+		for _, field := range fields {
+			if value, exists := item[field]; exists {
+				filteredItem[field] = value
+			}
+		}
+
+		filteredItems = append(filteredItems, filteredItem)
 	}
 
-	return result, nil
-
+	return filteredItems, nil
 }
